716525e1179196657378208bf6309eea63c17a96
EricDosReis <ericdosreis.personal@gmail.com>
2019-08-30 17:33:57 -0300

    Create File model and controller, associate User and File, add multer package and config, add gitkeep rule in gitignore

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/User';
 import SessionController from './app/controllers/Session';
+import FileController from './app/controllers/File';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -12,5 +18,7 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
