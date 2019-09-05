c86b40f2d17d509ab46d085a06a39fe889b1013c
EricDosReis <ericdosreis.personal@gmail.com>
2019-09-05 14:51:53 -0300

    Add nodemailer package, create mail config, create send mail

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '96bb7907309684',
+    pass: 'f77964f7b55d81',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
